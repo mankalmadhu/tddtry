59d9d0e246a82f0d64508fb90d49eb881067817b
Madhu <mankalmadhu@gmail.com>
2017-02-17 05:01:58 +0530

    more testd

--- tddtry/src/main/java/tddtry/even/PrimeFactorCalculator.java
+++ tddtry/src/main/java/tddtry/even/PrimeFactorCalculator.java
@@ -8,7 +8,7 @@ public class PrimeFactorCalculator {
 	List<Integer> getPrimeFactors(int input)
 	{
 		List<Integer> result = new ArrayList<Integer>();
-		if(input == 2)
+		if(input == 2 || input == 3)
 		{
 			result.add(2);
 		}
--- tddtry/src/test/java/tddtry/even/PrimeFactorCaclulatorTest.java
+++ tddtry/src/test/java/tddtry/even/PrimeFactorCaclulatorTest.java
@@ -2,6 +2,8 @@ package tddtry.even;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
@@ -13,12 +15,22 @@ public class PrimeFactorCaclulatorTest {
 		List<Integer> primeFactors = new PrimeFactorCalculator().getPrimeFactors(1);
 		assertTrue(primeFactors.isEmpty());
 	}
-	
+
 	@Test
 	public void testForTwo() {
 		List<Integer> primeFactors = new PrimeFactorCalculator().getPrimeFactors(2);
 		assertTrue(primeFactors.get(0) == 2);
 	}
 
+	@Test
+	public void testForThree() {
+		List<Integer> primeFactors = new PrimeFactorCalculator().getPrimeFactors(3);
+		assertTrue(primeFactors.get(0) == 2);
+	}
+
+	@Test
+	public void testForFour() {
+		assertEquals(new ArrayList<Integer>(Arrays.asList(2, 2)), new PrimeFactorCalculator().getPrimeFactors(4));
+	}
 
 }
