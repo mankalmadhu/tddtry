32a952bda227b01e6d9ada2433317321412c3626
mahobalan <mankalmadhu@gmail.com>
2017-02-22 15:47:01 +0100

    Working Prime Factor

--- tddtry/src/main/java/tddtry/even/PrimeFactorCalculator.java
+++ tddtry/src/main/java/tddtry/even/PrimeFactorCalculator.java
@@ -1,33 +1,74 @@
 package tddtry.even;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class PrimeFactorCalculator {
 
-	List<Integer> getPrimeFactors(int input) {
+	public List<Integer> getPrimeFactors(int input) {
+
+		if (input == 1) {
+			return new ArrayList<Integer>();
+		}
+
+		if (input == 2) {
+			return new ArrayList<Integer>(Arrays.asList(2));
+		}
+
+		if ((input % 2) == 0) {
+
+			return getPrimeFactorsForEvenNumbers(input);
+
+		} else {
+
+			return getPrimeFactorsForOddNumbers(input);
+		}
+
+	}
+
+	private List<Integer> getPrimeFactorsForEvenNumbers(int input) {
+
 		List<Integer> result = new ArrayList<Integer>();
 
-		if(input == 1){
-			return result;
-		}
 		while (!isPrime(input)) {
 			result.add(2);
 			input /= 2;
 		}
+
 		if (isPrime(input)) {
 			result.add(input);
 		}
 
 		return result;
+
+	}
+
+	private List<Integer> getPrimeFactorsForOddNumbers(int input) {
+
+		List<Integer> result = new ArrayList<Integer>();
+		if (isPrime(input)) {
+			result.add(input);
+		} else {
+			for (int i = 3; i <= (int) Math.sqrt(input); i += 2) {
+				if (isPrime(i) && (input % i) == 0) {
+					result.add(i);
+					input /= i;
+				}
+			}
+			result.add(input);
+		}
+
+		return result;
+
 	}
 
 	private boolean isPrime(int input) {
 
-		if(input == 2){
+		if (input == 2) {
 			return true;
 		}
-		if (input % 2 == 0){
+		if (input % 2 == 0) {
 			return false;
 		}
 		for (int i = 3; i * i <= input; i += 2) {
--- tddtry/src/test/java/tddtry/even/PrimeFactorCaclulatorTest.java
+++ tddtry/src/test/java/tddtry/even/PrimeFactorCaclulatorTest.java
@@ -14,32 +14,37 @@ import org.junit.runners.Parameterized;
 @RunWith(Parameterized.class)
 public class PrimeFactorCaclulatorTest {
 
-	public PrimeFactorCaclulatorTest(Integer inputNumber, List<Integer> expectedResult) {
+	public PrimeFactorCaclulatorTest(Integer inputNumber,
+			List<Integer> expectedResult) {
 		super();
 		this.input = inputNumber;
 		this.expectedResult = expectedResult;
 		this.primeFactorCalculator = new PrimeFactorCalculator();
 	}
-	
-	 @Parameterized.Parameters
-	   public static Collection<Object[]> primeNumbers() {
-	      return Arrays.asList(new Object[][] {
-	         { 1, new ArrayList<Integer>() },
-	         { 2, new ArrayList<Integer>(Arrays.asList(2))},
-	         { 3, new ArrayList<Integer>(Arrays.asList(3))},
-	         { 4, new ArrayList<Integer>(Arrays.asList(2,2))},
-	         { 5, new ArrayList<Integer>(Arrays.asList(5))},
-	         { 6, new ArrayList<Integer>(Arrays.asList(2,3))},
-	         { 7, new ArrayList<Integer>(Arrays.asList(7))},
-	         { 8, new ArrayList<Integer>(Arrays.asList(2,2,2))},
-	         { 9, new ArrayList<Integer>(Arrays.asList(3,3))}
-	      });
-	   }
 
+	@Parameterized.Parameters
+	public static Collection<Object[]> primeNumbers() {
+		return Arrays.asList(new Object[][]{{1, new ArrayList<Integer>()},
+				{2, new ArrayList<Integer>(Arrays.asList(2))},
+				{3, new ArrayList<Integer>(Arrays.asList(3))},
+				{4, new ArrayList<Integer>(Arrays.asList(2, 2))},
+				{5, new ArrayList<Integer>(Arrays.asList(5))},
+				{6, new ArrayList<Integer>(Arrays.asList(2, 3))},
+				{7, new ArrayList<Integer>(Arrays.asList(7))},
+				{8, new ArrayList<Integer>(Arrays.asList(2, 2, 2))},
+				{9, new ArrayList<Integer>(Arrays.asList(3, 3))},
+				{10, new ArrayList<Integer>(Arrays.asList(2, 5))},
+				{11, new ArrayList<Integer>(Arrays.asList(11))},
+				{12, new ArrayList<Integer>(Arrays.asList(2, 2, 3))},
+				{13, new ArrayList<Integer>(Arrays.asList(13))},
+				{14, new ArrayList<Integer>(Arrays.asList(2,7))},
+				{15, new ArrayList<Integer>(Arrays.asList(3,5))}});
+	}
 
 	@Test
 	public void testPrimeFactors() {
-		assertEquals(expectedResult, primeFactorCalculator.getPrimeFactors(input));
+		assertEquals(expectedResult,
+				primeFactorCalculator.getPrimeFactors(input));
 	}
 
 	private Integer input;
